022d75991a25995ad1f6a77feaa00e54602d5a07
rajabezwada <raja.java1972@gmail.com>
2020-09-15 16:26:16 +0530

    springBootApplication

---
+++ src/main/java/com/raja/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.raja.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class HelloController {
+	
+	@GetMapping(value = "/getHello")
+	public String getHello() {
+		String message = "my first spring boot applcation";
+		return message;
+	}
+	@GetMapping(value = "/getHello2")
+	public String getHello2() {
+		String message = "my second spring boot applcation";
+		return message;
+	}
+	@GetMapping(value = "/getHello3")
+	public String getHello3() {
+		String message = "my third spring boot applcation";
+		return message;
+	}
+
+}
